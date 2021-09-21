0228dda26c335735d4424cff0e3e01448ca117eb
Brett Taylor <brett@picwell.com>
2021-09-21 16:13:16 -0400

    add python 3.9

--- scripts/hadolint.sh
+++ scripts/hadolint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 while read -r file; do
-  echo $file
-  docker run --rm -i hadolint/hadolint < $file
+  echo "$file"
+  docker run --rm -i hadolint/hadolint < "$file"
 done <<< "$(find . -name Dockerfile)"
